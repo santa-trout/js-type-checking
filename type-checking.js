8e34d70b2d64c3c7bdf501dacb6cd51a1f8f6961
santa-trout <116644653+santa-trout@users.noreply.github.com>
2022-10-25 13:53:51 +0200

    Add files via upload

---
+++ type-checking.js
@@ -0,0 +1,162 @@
+/**
+ * @template T
+ * @param { unknown } value
+ * @param { (value: unknown) => value is T } typeGuard
+ * @return { asserts value is T }
+ * @throws { TypeError }
+ */
+export function assert(value, typeGuard) {
+    if (!typeGuard(value)) throw new TypeError();
+}
+
+/**
+ * @template { string | number | symbol } EntryKey
+ * @template EntryValue
+ * @param { EntryKey } entryKey
+ * @param { (entryValue: unknown) => entryValue is EntryValue } entryTypeGuard
+ */
+export function hasKey(entryKey, entryTypeGuard) {
+    /**
+     * @param { any } value
+     * @return { value is Record<EntryKey, EntryValue> }
+     */
+    return function hasKeyCheck(value) {
+        return typeof value === "object" && value != null && entryKey in value && entryTypeGuard(value[entryKey]);
+    };
+}
+
+/**
+ * @template { string | number | symbol } EntryKey
+ * @template EntryValue
+ * @param { EntryKey } entryKey
+ * @param { (entryValue: unknown) => entryValue is EntryValue } entryTypeGuard
+ */
+export function hasOptionalKey(entryKey, entryTypeGuard) {
+    /**
+     * @param { any } value
+     * @return { value is Partial<Record<EntryKey, EntryValue>> }
+     */
+    return function hasOptionalKeyCheck(value) {
+        return typeof value === "object" && value != null && (!(entryKey in value) || entryTypeGuard(value[entryKey]));
+    };
+}
+
+/**
+ * @template Item
+ * @param { (item: unknown) => item is Item } itemTypeGuard
+ */
+export function isArray(itemTypeGuard) {
+    /**
+     * @param { unknown } value
+     * @return { value is Array<Item> }
+     */
+    return function isArrayCheck(value) {
+        return Array.isArray(value) && value.every(itemTypeGuard);
+    };
+}
+
+/**
+ * @param { unknown } value
+ * @return { value is boolean }
+ */
+export function isBoolean(value) {
+    return typeof value === "boolean";
+}
+
+/**
+ * @param { unknown } value
+ * @return { value is true }
+ */
+export function isTrue(value) {
+    return value === true;
+}
+
+/**
+ * @param { unknown } value
+ * @return { value is false }
+ */
+export function isFalse(value) {
+    return value === false;
+}
+
+/**
+ * @template A
+ * @template B
+ * @param { (item: unknown) => item is A } typeGuardA
+ * @param { (item: unknown) => item is B } typeGuardB
+ */
+export function isEither2(typeGuardA, typeGuardB) {
+    /**
+     * @param { unknown } value
+     * @return { value is A | B }
+     */
+    return function isEither2Check(value) {
+        return typeGuardA(value) || typeGuardB(value);
+    };
+}
+
+/**
+ * @template A
+ * @template B
+ * @template C
+ * @param { (item: unknown) => item is A } typeGuardA
+ * @param { (item: unknown) => item is B } typeGuardB
+ * @param { (item: unknown) => item is C } typeGuardC
+ */
+export function isEither3(typeGuardA, typeGuardB, typeGuardC) {
+    /**
+     * @param { unknown } value
+     * @return { value is A | B | C }
+     */
+    return function isEither3Check(value) {
+        return typeGuardA(value) || typeGuardB(value) || typeGuardC(value);
+    };
+}
+
+/**
+ * @param { unknown } value
+ * @return { value is null }
+ */
+export function isNull(value) {
+    return value === null;
+}
+
+/**
+ * @param { unknown } value
+ * @return { value is number }
+ */
+export function isNumber(value) {
+    return typeof value === "number";
+}
+
+/**
+ * @param { unknown } value
+ * @return { value is string }
+ */
+export function isString(value) {
+    return typeof value === "string";
+}
+
+/**
+ * @param { unknown } value
+ * @return { value is undefined }
+ */
+export function isUndefined(value) {
+    return value === undefined;
+}
+
+/**
+ * @template T
+ * @param { (value: unknown) => T } throwingFunction
+ */
+export function throwing(throwingFunction) {
+    /**
+     * @param { unknown } value
+     * @return { value is T }
+     * @throws
+     */
+    return function throwingCheck(value) {
+        throwingFunction(value);
+        return true;
+    };
+}
---
+++ type-checking.test.js
@@ -0,0 +1,189 @@
+import * as TC from "./type-checking.js";
+
+/** @param { function(unknown): void } assert */
+function hasKeyTest(assert) {
+    assert(TC.hasKey("a", TC.isNumber)({ a: 123 }));
+    assert(!TC.hasKey("a", TC.isNumber)({ a: "123" }));
+    assert(!TC.hasKey("a", TC.isNumber)({ a: undefined }));
+    assert(!TC.hasKey("a", TC.isNumber)({ a: null }));
+    assert(!TC.hasKey("a", TC.isNumber)({}));
+
+    assert(TC.hasKey("a", TC.isUndefined)({ a: undefined }));
+    assert(!TC.hasKey("a", TC.isUndefined)({}));
+
+    assert(!TC.hasKey("a", TC.isNull)({ a: undefined }));
+    assert(!TC.hasKey("a", TC.isNull)({}));
+    assert(TC.hasKey("a", TC.isNull)({ a: null }));
+}
+
+/** @param { function(unknown): void } assert */
+function hasOptionalKeyTest(assert) {
+    assert(TC.hasOptionalKey("a", TC.isNumber)({ a: 123 }));
+    assert(!TC.hasOptionalKey("a", TC.isNumber)({ a: "123" }));
+    assert(!TC.hasOptionalKey("a", TC.isNumber)({ a: undefined }));
+    assert(!TC.hasOptionalKey("a", TC.isNumber)({ a: null }));
+    assert(TC.hasOptionalKey("a", TC.isNumber)({}));
+
+    assert(TC.hasOptionalKey("a", TC.isUndefined)({ a: undefined }));
+    assert(TC.hasOptionalKey("a", TC.isUndefined)({}));
+
+    assert(!TC.hasOptionalKey("a", TC.isNull)({ a: undefined }));
+    assert(TC.hasOptionalKey("a", TC.isNull)({}));
+    assert(TC.hasOptionalKey("a", TC.isNull)({ a: null }));
+}
+
+/** @param { function(unknown): void } assert */
+function isStringTest(assert) {
+    assert(!TC.isString({ a: 123 }));
+    assert(!TC.isString(123));
+    assert(!TC.isString(123.4));
+    assert(!TC.isString([""]));
+    assert(!TC.isString([]));
+    assert(!TC.isString(null));
+    assert(!TC.isString(undefined));
+    assert(!TC.isString(new String("")));
+    assert(TC.isString("123"));
+    assert(!TC.isString(true));
+    assert(!TC.isString(false));
+}
+
+/** @param { function(unknown): void } assert */
+function isNumberTest(assert) {
+    assert(!TC.isNumber({ a: 123 }));
+    assert(TC.isNumber(123));
+    assert(TC.isNumber(123.4));
+    assert(!TC.isNumber([""]));
+    assert(!TC.isNumber([]));
+    assert(!TC.isNumber(null));
+    assert(!TC.isNumber(undefined));
+    assert(!TC.isNumber(new String("")));
+    assert(!TC.isNumber("123"));
+    assert(!TC.isNumber(true));
+    assert(!TC.isNumber(false));
+}
+
+/** @param { function(unknown): void } assert */
+function isBooleanTest(assert) {
+    assert(!TC.isBoolean({ a: 123 }));
+    assert(!TC.isBoolean(123));
+    assert(!TC.isBoolean(123.4));
+    assert(!TC.isBoolean([""]));
+    assert(!TC.isBoolean([]));
+    assert(!TC.isBoolean(null));
+    assert(!TC.isBoolean(undefined));
+    assert(!TC.isBoolean(new String("")));
+    assert(!TC.isBoolean("123"));
+    assert(TC.isBoolean(true));
+    assert(TC.isBoolean(false));
+}
+
+/** @param { function(unknown): void } assert */
+function isNullTest(assert) {
+    assert(!TC.isNull({ a: 123 }));
+    assert(!TC.isNull(123));
+    assert(!TC.isNull(123.4));
+    assert(!TC.isNull([""]));
+    assert(!TC.isNull([]));
+    assert(TC.isNull(null));
+    assert(!TC.isNull(undefined));
+    assert(!TC.isNull(new String("")));
+    assert(!TC.isNull("123"));
+    assert(!TC.isNull(true));
+    assert(!TC.isNull(false));
+}
+
+/** @param { function(unknown): void } assert */
+function isArrayTest(assert) {
+    assert(!TC.isArray(TC.isNull)({ a: 123 }));
+    assert(!TC.isArray(TC.isNull)(123));
+    assert(!TC.isArray(TC.isNull)(123.4));
+    assert(!TC.isArray(TC.isNull)([""]));
+    assert(TC.isArray(TC.isNull)([]));
+    assert(TC.isArray(TC.isString)([]));
+    assert(TC.isArray(TC.isString)([""]));
+    assert(!TC.isArray(TC.isNumber)([""]));
+    assert(!TC.isArray(TC.isNull)(null));
+    assert(!TC.isArray(TC.isNull)(undefined));
+    assert(!TC.isArray(TC.isNull)(new String("")));
+    assert(!TC.isArray(TC.isNull)("123"));
+    assert(!TC.isArray(TC.isString)("123"));
+    assert(!TC.isArray(TC.isNull)(true));
+    assert(!TC.isArray(TC.isNull)(false));
+}
+
+/** @param { function(unknown): void } assert */
+function isEither2Test(assert) {
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)({ a: 123 }));
+    assert(TC.isEither2(TC.isNull, TC.isNumber)(123));
+    assert(TC.isEither2(TC.isNull, TC.isNumber)(123.4));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)([""]));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)([]));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)([]));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)([""]));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)([""]));
+    assert(TC.isEither2(TC.isNull, TC.isNumber)(null));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)(undefined));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)(new String("")));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)("123"));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)("123"));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)(true));
+    assert(!TC.isEither2(TC.isNull, TC.isNumber)(false));
+}
+
+/** @param { function(unknown): void } assert */
+function isEither3Test(assert) {
+    assert(!TC.isEither3(TC.isNull, TC.isNumber, TC.isString)({ a: 123 }));
+    assert(TC.isEither3(TC.isNull, TC.isNumber, TC.isString)(123));
+    assert(TC.isEither3(TC.isNull, TC.isNumber, TC.isString)(123.4));
+    assert(!TC.isEither3(TC.isNull, TC.isNumber, TC.isString)([""]));
+    assert(!TC.isEither3(TC.isNull, TC.isNumber, TC.isString)([]));
+    assert(!TC.isEither3(TC.isNull, TC.isNumber, TC.isString)([]));
+    assert(!TC.isEither3(TC.isNull, TC.isNumber, TC.isString)([""]));
+    assert(!TC.isEither3(TC.isNull, TC.isNumber, TC.isString)([""]));
+    assert(TC.isEither3(TC.isNull, TC.isNumber, TC.isString)(null));
+    assert(!TC.isEither3(TC.isNull, TC.isNumber, TC.isString)(undefined));
+    assert(!TC.isEither3(TC.isNull, TC.isNumber, TC.isString)(new String("")));
+    assert(TC.isEither3(TC.isNull, TC.isNumber, TC.isString)("123"));
+    assert(!TC.isEither3(TC.isNull, TC.isNumber, TC.isString)(true));
+    assert(!TC.isEither3(TC.isNull, TC.isNumber, TC.isString)(false));
+}
+
+/** @param { unknown } value */
+function addressOrThrow(value) {
+    TC.assert(value, TC.hasKey("street", TC.isString));
+    TC.assert(value, TC.hasKey("houseNumber", TC.isNumber));
+    return value;
+}
+
+/** @typedef { ReturnType<typeof addressOrThrow> } Address */
+
+/** @param { unknown } value */
+function personOrThrow(value) {
+    TC.assert(value, TC.hasKey("firstName", TC.isString));
+    TC.assert(value, TC.hasKey("lastName", TC.isString));
+    TC.assert(value, TC.hasOptionalKey("middleName", TC.isString));
+    TC.assert(value, TC.hasKey("addresses", TC.isArray(TC.throwing(addressOrThrow))));
+    return value;
+}
+
+/** @typedef { ReturnType<typeof personOrThrow> } Person */
+
+/** @param { function } f */
+function isThrowing(f) {
+    try { f(); return false; } catch (e) { return true; }
+}
+
+/** @param { function(unknown): void } assert */
+function runTests(assert) {
+    hasKeyTest(assert);
+    hasOptionalKeyTest(assert);
+    isArrayTest(assert);
+    isBooleanTest(assert);
+    isEither2Test(assert);
+    isEither3Test(assert);
+    isNullTest(assert);
+    isNumberTest(assert);
+    isStringTest(assert);
+}
+
+runTests(result => { if (!result) throw new Error(); });
